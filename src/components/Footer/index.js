aae5be9c275072234a42f94b3d57f2098bf30039
Théodore Bing <theodorebing@gmail.com>
2021-08-11 20:12:17 +0200

    add responsiveness and fixe links on footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,13 +5,13 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
-        <a href="www.linkedin.com/in/theodorebing">
+        <a href="https://www.linkedin.com/in/theodorebing" target="_blank" rel="noreferrer">
           <img className="footer-icon" src="https://img.icons8.com/color/480/000000/linkedin.png" alt="Linkedin" />
         </a>
-        <a href="https://github.com/theodorebing">
+        <a href="https://github.com/theodorebing" target="_blank" rel="noreferrer">
           <img className="footer-icon" src="https://img.icons8.com/fluency/240/000000/github.png" alt="Github" />
         </a>
-        <a href="https://twitter.com/BingTheodore">
+        <a href="https://twitter.com/BingTheodore" target="_blank" rel="noreferrer">
           <img className="footer-icon" src="https://img.icons8.com/fluency/240/000000/twitter.png" alt="Twitter" />
         </a>
       </div>

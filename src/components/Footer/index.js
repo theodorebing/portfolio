f36a514a6a5ef3af8346ac000a554ce01a21d8af
Théodore Bing <theodorebing@gmail.com>
2021-08-11 19:38:04 +0200

    footer finished with social networks logos, links and effects

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,17 @@ const Footer = () => {
   console.log('test');
   return (
     <footer className="footer">
-      <h1>Footer</h1>
+      <div className="footer-container">
+        <a href="www.linkedin.com/in/theodorebing">
+          <img className="footer-icon" src="https://img.icons8.com/color/480/000000/linkedin.png" alt="Linkedin" />
+        </a>
+        <a href="https://github.com/theodorebing">
+          <img className="footer-icon" src="https://img.icons8.com/fluency/240/000000/github.png" alt="Github" />
+        </a>
+        <a href="https://twitter.com/BingTheodore">
+          <img className="footer-icon" src="https://img.icons8.com/fluency/240/000000/twitter.png" alt="Twitter" />
+        </a>
+      </div>
     </footer>
   );
 };
